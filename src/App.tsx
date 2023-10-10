e9f7bc899acc2458d641b97a06f081b9cf7d36e2
Hiiiken <hiiiken@live.com>
2023-10-10 22:23:39 +0100

    Styling the form component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,19 @@ import Form from "./components/Form";
 
 const App = () => {
    return (
-      <>
-         <h1 className="text-4xl mb-8">URL Shortener</h1>
-         <Form />
-      </>
+      <div className="bg-gradient-to-r from-gray-950 to-gray-900 h-screen flex justify-center content-center flex-wrap p-10 sm:p-20">
+         <div className="w-[720px] lg:[720px] p-8 border rounded-lg border-cyan-700">
+            <h1 className="text-4xl mb-4 text-white font-black ">
+               URL SHORTENER
+            </h1>
+            <p className="text-white mb-8 text-base">
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+               quod! Similique adipisci sit excepturi illo dolor voluptatibus
+               officiis placeat quibusdam.
+            </p>
+            <Form />
+         </div>
+      </div>
    );
 };
 
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -10,6 +10,7 @@ const Form = () => {
    const [responseData, setResponseData] = useState<ApiResponse | null>(null);
    const [isLoading, setIsLoading] = useState(false);
    const [error, setError] = useState<string | null>(null);
+   const [isVisible, setIsVisible] = useState(false);
 
    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       setInputValue(e.target.value);
@@ -17,6 +18,7 @@ const Form = () => {
 
    const handleButtonClick = () => {
       fetchData(inputValue);
+      setIsVisible(true);
    };
 
    const customErrorMessages: { [key: number]: string } = {
@@ -50,36 +52,48 @@ const Form = () => {
 
    return (
       <div>
-         <h2 className="text-xl font-semibold mb-4">URL Shortener</h2>
-         <input
-            className="py-3 px-4 border border-orange-400"
-            type="text"
-            placeholder="Enter your link here..."
-            value={inputValue}
-            onChange={handleInputChange}
-         />
-         <button
-            className="py-3 px-4 ml-4 bg-orange-500 border border-orange-400 text-white font-semibold"
-            onClick={handleButtonClick}
-         >
-            Shorten URL
-         </button>
-
-         <div>
-            {isLoading ? (
-               <p>Loading...</p>
-            ) : error ? (
-               <div>
-                  There is an error:
-                  <p className="font-bold">{error}</p>
-               </div>
-            ) : responseData ? (
-               <div>
-                  The short code is:
-                  <p className="font-bold">{responseData.result.short_link}</p>
-               </div>
-            ) : null}
+         <div className="bg-gray-900 rounded-lg p-8">
+            <h2 className="text-xl text-white font-semibold mb-4">
+               Enter your link
+            </h2>
+            <div className="flex flex-wrap lg:flex-nowrap gap-4">
+               <input
+                  className="py-3 px-4 w-full lg:w-2/3 border rounded bg-gray-800 border-gray-800 text-white placeholder:text-gray-400 hover:border-cyan-500 hover:bg-gray-900 focus:outline-none focus:border-cyan-500 focus:bg-gray-900 active:bg-gray-900 transition ease-in-out duration-500"
+                  type="text"
+                  placeholder="Enter your link here..."
+                  value={inputValue}
+                  onChange={handleInputChange}
+               />
+               <button
+                  className="py-3 px-4 w-full lg:w-1/3 rounded bg-gradient-to-r from-cyan-500 to-cyan-300   text-gray-950 font-semibold hover:bg-gradient-to-r hover:from-cyan-300 hover:to-cyan-500"
+                  onClick={handleButtonClick}
+               >
+                  Shorten URL
+               </button>
+            </div>
          </div>
+
+         {isVisible && (
+            <div className="bg-gray-900 rounded-lg p-8 mt-4">
+               {isLoading ? (
+                  <p className="font-normal text-green-400">Loading...</p>
+               ) : error ? (
+                  <div>
+                     <p className="text-red-400">There is an error:</p>
+                     <p className="font-normal text-white">{error}</p>
+                  </div>
+               ) : responseData ? (
+                  <div>
+                     <p className="font-normal text-cyan-400">
+                        The short code is:
+                     </p>
+                     <p className="font-normal text-white">
+                        {responseData.result.short_link}
+                     </p>
+                  </div>
+               ) : null}
+            </div>
+         )}
       </div>
    );
 };
@@ -87,5 +101,6 @@ const Form = () => {
 export default Form;
 
 // TODO:
-// Add CSS. Deploy.
+// Add CSS.
+// Create separate components for 'Displaying' and Errors?
 // Add more validation to make sure the user enters a valid URL
